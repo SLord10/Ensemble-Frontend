260123042f43edf45d8f41a32a701eb9af72aef3
Achraf BOUKIR <105467441+Da-achraf@users.noreply.github.com>
2024-01-07 18:52:51 +0100

    Updates

--- src/app/config/backend_api.config.ts
+++ src/app/config/backend_api.config.ts
@@ -1,6 +1,6 @@
 import {InjectionToken} from "@angular/core";
 
-const API_URL = "http://localhost:8083"
+const API_URL = "http://localhost"
 
 export const API_TOKEN = new InjectionToken("API_URL", {
     providedIn: 'root',
